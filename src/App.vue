f7fa700b24b84dd5640873917095a4062e65e35b
Mohammad Moghari <mohamadmoghari@hotmail.com>
2021-05-15 08:39:02 +0430

    1400.02.25

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,88 @@
 <template>
-
   <div id="app">
     <div id="header">
       <button @click="addItem">Add New Item</button>
     </div>
+    <br />
     <div v-for="item in todoItems" :key="item.id">
-    <todoItem  v-on:cildToParent="saveData" :key="item.id" :id="item.id" :message="item.message"/>
+      <todoItem
+        v-on:saveToParent="saveData"
+        v-on:deleteToParent="deleteData"
+        v-on:markToParent="markItem"
+        :key="item.id"
+        :id="item.id"
+        :mark="item.markAsDone"
+        :message="item.message"
+        :saved="item.saved"
+      />
     </div>
   </div>
 </template>
 
 <script>
-
-import TodoItem from './components/todoItem.vue';
+import TodoItem from "./components/todoItem.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    todoItem:TodoItem,
+    todoItem: TodoItem,
   },
   data() {
     return {
-      todoItems:[]
-    }
+      todoItems: [],
+    };
   },
   methods: {
+    /********************************* */
+    setConsole(message) {
+      console.log(message);
+    },
+
+    getGuid() {
+      return Math.floor(Math.random() * 0x10000).toString(16);
+    },
+    getArrayIndex(id) {
+      for (let index = 0; index < this.todoItems.length; index++) {
+        if (this.todoItems[index].id === id) {
+          return index;
+        }
+      }
+      return -1;
+    },
+
+    /*********************************8 */
+
     addItem() {
       this.todoItems.push({
-        id:this.todoItems.length+1,
-        message:''
+        id: this.getGuid(), //this.todoItems.length+1,
+        message: "",
+        markAsDone: false,
+        saved:false
       });
     },
-    saveData(id, message)
-    {
-     this.todoItems[id-1].message=message
-    }
+    saveData(id, message) {
+      let index = this.getArrayIndex(id);
+      if (index >= 0) {
+        this.todoItems[index].message = message;
+        this.todoItems[index].saved=true;
+      }
+    },
+    deleteData(id) {
+      let deletedIndex = this.getArrayIndex(id);
+      this.setConsole(deletedIndex);
+      if (deletedIndex >= 0) this.todoItems.splice(deletedIndex,1);
+    },
+    markItem(id, check) {
+      let index = this.getArrayIndex(id);
+      if (index >= 0) this.todoItems[index].markAsDone = check;
+    },
   },
-}
+};
 </script>
 <style scoped>
-#div1{
-width:50px;
-height: 50px;
-background-color: yellow;
+#div1 {
+  width: 50px;
+  height: 50px;
+  background-color: yellow;
 }
 </style>
--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -1,9 +1,23 @@
  <template>
   <div>
-      <textarea id="txt" cols="30" rows="10" v-model="innerMessage"></textarea>
-      <br>
-      <button @click="saveData(innerId,innerMessage)">save</button>
+    <div id="divTxt">
+      <textarea id="txt" cols="10" rows="8" v-model="innerMessage"></textarea>
     </div>
+    <div v-show="innerSaved" id="divButton">
+      <button @click="deleteData(innerId)">delete</button>
+      <br />
+      <div v-if="innerMark === false">
+        <button @click="markData">mark as done</button>
+      </div>
+      <div v-else>
+        <button @click="markData" style="background: green">
+          mark as done
+        </button>
+      </div>
+    </div>
+    <br />
+    <button @click="saveData(innerId, innerMessage)">save</button>
+  </div>
 </template>
 
 <script>
@@ -11,20 +25,38 @@ export default {
   data() {
     return {
       innerMessage: this.message,
-      innerId:this.id
-    }
+      innerId: this.id,
+      innerMark: this.mark,
+      innerSaved: this.saved,
+    };
   },
   methods: {
-    saveData(i,m) {
-      
-      this.$emit('cildToParent',i,m)
-    }
+    /**************************** */
+    setConsole(message) {
+      console.log(message);
+    },
+    /**************************** */
+    saveData(i, m) {
+      this.innerSaved = true;
+      this.$emit("saveToParent", i, m);
+    },
+    deleteData(i) {
+      this.$emit("deleteToParent", i);
+    },
+    markData() {
+      this.innerMark = !this.innerMark;
+      this.$emit("markToParent", this.innerId, this.innerMark);
+    },
   },
-props: ["message","id"]
-
-}
+  props: ["message", "id", "mark", "saved"],
+};
 </script>
 
 <style>
-
+#divTxt {
+  display: inline-flex;
+}
+#divButton {
+  display: inline-flex;
+}
 </style>
